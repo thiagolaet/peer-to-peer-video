1d51044e3eadaa97ec25099bc59876df3361cc10
Antonio Romano <antonio.romano@quantumfinance.com.br>
2023-10-30 21:37:04 -0300

    Retorna HOST inicial

--- client/client.py
+++ client/client.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = "127.0.0.10"  # The server's hostname or IP address
+HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 delimiter = '0111110'
--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 import socket
 from repositories.user_repository import UserRepository
 
-HOST = "127.0.0.10"  # Standard loopback interface address (localhost)
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports  are > 1023)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
